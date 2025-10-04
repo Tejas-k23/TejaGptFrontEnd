e76e78c13828aa491d012205b8e187490ae3615a
Tejas <tejaskumbharkar6398@gmail.com>
2025-10-04 14:03:38 +0530

    e fixed

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,8 @@
 const IS_PROD = true;
 
-const server = IS_PROD
-  ? "https://tejagpt.onrender.com"   // no trailing slash
-  : "http://localhost:8080";
+const server = (IS_PROD 
+  ? "https://tejagpt.onrender.com" 
+  : "http://localhost:8080"
+).replace(/\/$/, ""); // removes any trailing slash
 
 export default server;
